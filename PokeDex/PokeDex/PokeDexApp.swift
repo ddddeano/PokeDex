42ae38387250ee6c7c2bf1789924bcae90e6f73f
Daniel Watson <55237431+ddddeano@users.noreply.github.com>
2021-01-11 10:58:14 +0100

    Add files via upload
    Empty Starter Project for PokeDex

---
+++ PokeDex/PokeDex/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//
+//  Created by Daniel Watson on 11/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
